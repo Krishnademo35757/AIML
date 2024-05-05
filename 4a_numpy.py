b93dbfbecb47b7d62b668356c465a7716654e87b
Krishnademo35757 <168057311+Krishnademo35757@users.noreply.github.com>
2024-05-05 16:32:37 +0530

    Add files via upload

---
+++ 4a_numpy.py
@@ -0,0 +1,10 @@
+#pip install numpy
+
+import numpy as np
+
+# Example usage
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
+print(np.mean(arr))     # Mean of array
+print(np.sum(arr))      # Sum of array
+print(np.max(arr))      # Maximum value in array
